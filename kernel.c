3852a243ebfbf378171251bbe40829b6e67a3261
Brad Holmes <inkspotch@gmail.com>
2017-12-26 17:20:44 -0600

    Recognize newlines in tty

--- kernel.c
+++ kernel.c
@@ -2,11 +2,13 @@
 #include "vga.h"
 
 void kmain(void) {
-  char *str = "Color kernel. :D";
-  terminal_init();
-  terminal_set_color(vga_color(red, white));
-  terminal_write(str);
+    terminal_init();
+    char* str = "Booting kernel.\n";
+    terminal_set_color(vga_color(red, white));
+    terminal_write(str);
+
+    str = "New lines are now accepted by the tty.\n";
+    terminal_set_color(vga_color(light_blue, dark_grey));
+    terminal_write(str);
 }
-
-
 
--- tty.c
+++ tty.c
@@ -10,7 +10,7 @@ void terminal_init(void) {
     terminal_row = 0;
     terminal_col = 0;
     terminal_color = vga_color(light_grey, black);
-    terminal_buffer = (uint16_t*) 0xb8000;
+    terminal_buffer = (uint16_t *) 0xb8000;
 
     for (size_t r = 0; r < ROWS; r++) {
         for (size_t c = 0; c < COLS; c++) {
@@ -20,20 +20,29 @@ void terminal_init(void) {
     }
 }
 
-void terminal_write(const char* data) {
+void terminal_write(const char *data) {
     while (*data) {
-        size_t index = terminal_row * COLS + terminal_col;
-        terminal_buffer[index] = vga_entry(*data, terminal_color);
-
-        terminal_col += 1;
-
-        if (terminal_col == COLS) {
+        if (*data == '\n') {
             terminal_col = 0;
-
             terminal_row += 1;
 
-            if (terminal_row == ROWS) {
+            if (terminal_row >= ROWS) {
                 terminal_row = 0;
+            }
+        } else {
+            size_t index = terminal_row * COLS + terminal_col;
+            terminal_buffer[index] = vga_entry(*data, terminal_color);
+
+            terminal_col += 1;
+
+            if (terminal_col == COLS) {
+                terminal_col = 0;
+
+                terminal_row += 1;
+
+                if (terminal_row == ROWS) {
+                    terminal_row = 0;
+                }
             }
         }
 
